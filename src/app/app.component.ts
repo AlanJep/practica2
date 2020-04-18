f812bc9f210e9b69e3ec6e0200ea635caa31d7b8
AlanJep <alanjavierespejoprado627@gmail.com>
2020-04-17 21:09:59 -0500

    version3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,18 @@
 import { Component } from '@angular/core';
-import { UsuariosService } from './usuarios.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: "./app.component.html",
-  styles: ['./app.component.scss'],
-  providers : [
-    UsuariosService
-  ]
+  styles: ['./app.component.scss']
 })
 export class AppComponent {
 
-  personas : any =[];
+  constructor(){}
 
-    constructor(private usuarioservice : UsuariosService){
-      this.personas = usuarioservice.getUsuarios();
-    }
+  miFormulario = new FormGroup({
+    username: new FormControl('', Validators.required),
+    userpassword: new FormControl('', Validators.required)
+  });
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
